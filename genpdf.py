f050cb1ad12adc87a6db1ecb60a13b39d0ce5522
Tebski <tebanevans@gmail.com>
2018-10-11 16:30:27 +0000

    Cleaned up code and added comment section. Modified to use current directory as script running directory

--- genpdf.py
+++ genpdf.py
@@ -1,10 +1,32 @@
+'''
+Script to process .txt files from glovia, and convert to PDF
+
+Version 1.2
+Added ability to split txt file based on vendor, as page breaks from the text file appear unreliableself.
+Suppress first line as it contains a page number which could be confusing to the end user
+
+Version 1.1
+Separated files into subfolders to make directory cleaner.
+Removed last page if its blank to prevent empty pdfs being created
+
+Version 1.0
+Initial concept. Split txt files from Glovia based on page breaks. One page per vendor. Produce a PDF for each page named after the vendor and the orginal date/time stamp of the text fileself.
+Naming conventions:
+Input file is of type .txt and named ACHStubs_<date/time stamp>.txt
+Output file is of type .pdf and named <vendor>_<date/time stamp.pdf
+Script can process multiple input files, and will work so long as date/time stamp is unique for each to prevent conflicts with same vendor being in each.
+
+Vendor must appear in the page of the text file.
+'''
+
+
 from fpdf import FPDF
 import os
 
 attach_sub_dir = 'attachments'
 archive_sub_dir = 'archive'
 txt_sub_dir = 'text'
-home_dir = os.path.join(os.path.curdir,"stubs")
+home_dir = os.path.curdir
 dir_to_scan = os.path.join(home_dir,txt_sub_dir)
 dir_archive = os.path.join(home_dir,archive_sub_dir)
 dir_attach = os.path.join(home_dir,attach_sub_dir)
@@ -21,7 +43,6 @@ for f in files:
     edateindex = f.find(".txt")
 
     if edateindex == -1 or bdateindex == -1:
-        #print("No .txt file extension on this one")
         valid_file = False
     else:
         valid_file = True
@@ -31,7 +52,6 @@ for f in files:
         print(idate)
         with open(os.path.join(dir_to_scan,f),"r") as ifile:
             txt = ifile.read()
-            #print(txt)
             #check if it contains multiple pages
             if txt.count('MDC  C_ACHSTB') > 1:
                 #multiple headers found
@@ -53,7 +73,6 @@ for f in files:
                 pages.pop()
 
             for page in pages:
-                #print(page)
                 venindex = page.find("Ven: ")
                 if venindex == -1:
                     print("no vendor found in " + f)
@@ -67,4 +86,3 @@ for f in files:
                     pdf.multi_cell(0,5,page,0,"L")
                     pdf.output(os.path.join(dir_attach,vendor +'.pdf'),"F")
         os.rename(os.path.join(dir_to_scan,f),os.path.join(dir_archive,f))
-        #if (f.endswith(".txt")):
