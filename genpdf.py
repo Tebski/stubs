6b6b26296708ef023cadaf015233feee9138c20f
Steve Evans <steve@fixajob.com>
2018-11-17 18:03:35 -0800

    Version 1.4: Added support for multiple pages of stubs for single vendor (#3)

--- genpdf.py
+++ genpdf.py
@@ -1,5 +1,12 @@
 '''
 Script to process .txt files from glovia, and convert to PDF
+Version 1.4 - SRE
+Store pages without a vendor to be collated with a page that has a vendor to handle
+scenario of vendor with multiple pages of stubs
+
+Version 1.3 - SRE
+Removed assumption on CODIV being MDC. Search now only looks for the report function
+name to find the page headers.
 
 Version 1.2 - SRE
 Added ability to split txt file based on vendor, as page breaks from the text
@@ -29,76 +36,91 @@ Vendor must appear in the page of the text file.
 
 import os
 from fpdf import FPDF
-import os
 
-attach_sub_dir = 'attachments'
-archive_sub_dir = 'archive'
-txt_sub_dir = 'text'
-home_dir = os.path.join(os.path.curdir,"stubs")
-dir_to_scan = os.path.join(home_dir,txt_sub_dir)
-dir_archive = os.path.join(home_dir,archive_sub_dir)
-dir_attach = os.path.join(home_dir,attach_sub_dir)
-files = os.listdir(dir_to_scan)
-valid_file = True
+ATTACH_SUB_DIR = 'attachments'
+ARCHIVE_SUB_DIR = 'archive'
+TXT_SUB_DIR = 'text'
+HOME_DIR = os.path.curdir
+DIR_TO_SCAN = os.path.join(HOME_DIR, TXT_SUB_DIR)
+DIR_ARCHIVE = os.path.join(HOME_DIR, ARCHIVE_SUB_DIR)
+DIR_ATTACH = os.path.join(HOME_DIR, ATTACH_SUB_DIR)
 
 
+print("Scanning: " + DIR_TO_SCAN)
+def main():
+    files = os.listdir(DIR_TO_SCAN)
+    valid_file = True
 
+    # Process files
+    for f in files:
+        print("processing: " + f)
+        bdateindex = f.find("ACHStubs_")
+        edateindex = f.find(".txt")
 
-# Process files
-for f in files:
-    print("processing: " + f)
-    bdateindex = f.find("ACHStubs_")
-    edateindex = f.find(".txt")
+        if edateindex == -1 or bdateindex == -1:
+            valid_file = False
+        else:
+            valid_file = True
 
-    if edateindex == -1 or bdateindex == -1:
-        #print("No .txt file extension on this one")
-        valid_file = False
-    else:
-        valid_file = True
+        if valid_file:
+            idate = f[bdateindex+9:edateindex]
+            #print(idate)
+            with open(os.path.join(DIR_TO_SCAN, f), "r") as ifile:
+                txt = ifile.read()
+                
+                #check if it contains multiple pages
+                if txt.count('C_ACHSTB') > 1:
+                    #multiple headers found
+                    if txt.count('\f') == 0:
+                        #no page breaks found, need to insert them
+                        start = 10 #ignore first occurence
+                        pos = 1
+                        while pos < len(txt) and pos > 0:
+                            pos = (txt.find('C_ACHSTB', start))
+                            #print("position: " + str(pos))
+                            if pos > -1:
+                                txt = txt[:pos-6] + '\f' + txt[pos-5:]
+                                start = pos+1
 
-    if valid_file:
-        idate = f[bdateindex+9:edateindex]
-        print(idate)
-        with open(os.path.join(dir_to_scan,f),"r") as ifile:
-            txt = ifile.read()
-            #print(txt)
-            #check if it contains multiple pages
-            if txt.count('MDC  C_ACHSTB') > 1:
-                #multiple headers found
-                if txt.count('\f') == 0:
-                    #no page breaks found, need to insert them
-                    start = 10 #ignore first occurence
-                    pos = 1
-                    while pos < len(txt) and pos > 0:
-                        pos = txt.find('MDC  C_ACHSTB',start)
-                        if pos > -1:
-                            txt = txt[:pos-1] + '\f' + txt[pos:]
-                            start = pos+1
+                #split on page break
+                pages = txt.split("\f")
+                #remove last page (if it's blank)
+                if len(pages[-1].strip()) == 0:
+                    print("Empty page being ignored")
+                    pages.pop()
 
-            #split on page break
-            pages = txt.split("\f")
-            #remove last page (if it's blank)
-            if len(pages[-1].strip()) == 0:
-                print("Empty page being ignored")
-                pages.pop()
+                ven_pages = []
 
-            for page in pages:
-                #print(page)
-                venindex = page.find("Ven: ")
-                if venindex == -1:
-                    print("no vendor found in " + f)
-                else:
-                    vendor = page[venindex+5:venindex+11].strip()
-                    if not vendor:
-                        print("Invalid vendor found in " + f)
+                for page in pages:
+                    page = page[page.find('\n')+1:] #suppress first line
+                    venindex = page.find("Ven: ")
+                    if venindex == -1:
+                        ven_pages.append(page)
+                        print("multipage vendor found")
                     else:
-                        print("Processing vendor: " + vendor)
-                        page = page[page.find('\n')+1:] #suppress first line
-                        pdf = FPDF(format="letter")
-                        pdf.add_page()
-                        pdf.set_font("Courier", '', 8)
-                        pdf.multi_cell(0, 5, page, 0, "L")
-                        pdf.output(os.path.join(DIR_ATTACH, vendor + "_" + idate + '.pdf'), "F")
-        os.rename(os.path.join(DIR_TO_SCAN, f), os.path.join(DIR_ARCHIVE, f))
-    else:
-        print("Invalid file name " + f + " found in " + DIR_TO_SCAN)
+                        vendor = page[venindex+5:venindex+11].strip()
+                        if not vendor:
+                            print("Invalid vendor found in " + f)
+                        else:
+                            print("Processing vendor: " + vendor)
+                            ven_pages.append(page)
+                            pdf = FPDF(format="letter")
+                            for ven_page in ven_pages:
+                                pdf.add_page()
+                                pdf.set_font("Courier", '', 8)
+                                pdf.multi_cell(0, 4, ven_page, 0, "L")
+                            pdf.output(os.path.join(DIR_ATTACH, vendor + "_" + idate + '.pdf'), "F")
+                            ven_pages.clear()
+            try:
+                os.rename(os.path.join(DIR_TO_SCAN, f), os.path.join(DIR_ARCHIVE, f))
+            except FileExistsError as e:
+                print("File " + f + " already exists in " + DIR_ARCHIVE + ". Attempting cleanup.")
+                os.remove(os.path.join(DIR_ARCHIVE, f))
+                os.rename(os.path.join(DIR_TO_SCAN, f), os.path.join(DIR_ARCHIVE, f))
+
+        else:
+            print("Invalid file name " + f + " found in " + DIR_TO_SCAN)
+
+
+if __name__ == '__main__':
+    main()
